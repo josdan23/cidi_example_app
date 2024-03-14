a3cb4fc48187546e8fb2af7783f1b81da59e53f2
DaniYapuraExtrados <danielyapura@gmail.com>
2024-03-14 19:43:53 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# cidi_example_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/cidi_example_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.cidi_example_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
