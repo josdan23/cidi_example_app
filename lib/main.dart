a7970f6134da0615a53c6cc4f0c7ad13887fbf4a
DaniYapuraExtrados <133370362+DaniYapuraExtrados@users.noreply.github.com>
2024-03-15 20:09:30 -0300

    se borro algo

--- lib/main.dart
+++ lib/main.dart
@@ -43,7 +43,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times muchas:',
+              'You have pushed the button this many :',
             ),
             Text(
               '$_counter',
